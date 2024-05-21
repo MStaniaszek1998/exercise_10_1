35682687862f8b0e50c3d81647ee4d4da157d18b
MS <mateusz.staniaszek1998@gmail.com>
2024-05-21 16:32:03 +0200

    Change function to be generic and accept float/int

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/MStaniaszek1998/exercise_10_1.git
 
 go 1.22.2
+
+require golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842
--- modpack.go
+++ modpack.go
@@ -1,10 +1,17 @@
 package exercise101
 
+import (
+	"golang.org/x/exp/constraints"
+)
 
-// Add sums up two Integer numbers
-// It has two parameters: a - which is a first int and b - which is a second
+type Number interface {
+	constraints.Float | constraints.Integer
+}
+
+// Add sums up two Integer or Float numbers and can return float or integer respectively
+// It has two parameters: a - which is a first int or float and b - which is a second int or float
 // Great website [MathIsFun]
 // [MathIsFun]: https://www.mathsisfun.com/numbers/addition.html
-func Add(a,b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
 }
