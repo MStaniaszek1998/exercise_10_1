3edae04e554894859a00af36bec42fc4a89de379
MS <mateusz.staniaszek1998@gmail.com>
2024-05-21 16:25:17 +0200

    Add documentation

--- modpack.go
+++ modpack.go
@@ -1,7 +1,10 @@
 package exercise101
 
 
-
+// Add sums up two Integer numbers
+// It has two parameters: a - which is a first int and b - which is a second
+// Great website [MathIsFun]
+// [MathIsFun]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a,b int) int {
 	return a + b
 }
